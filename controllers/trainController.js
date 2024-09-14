1a134ccc5202b31b758aad1d69c687c20a898576
Pamuditha Wijerathne <pamuditha.lak@gmail.com>
2024-09-15 00:10:27 +0530

    swagger api spec added

--- controllers/trainController.js
+++ controllers/trainController.js
@@ -1,6 +1,43 @@
 const TrainLocation = require('../models/TrainLocation');
 
-// Add train location
+/**
+ * @swagger
+ * /locations:
+ *   post:
+ *     summary: Add a new train location
+ *     description: Add a new train location to the database
+ *     tags: [TrainLocation]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               trainId:
+ *                 type: string
+ *               latitude:
+ *                 type: number
+ *                 format: float
+ *               longitude:
+ *                 type: number
+ *                 format: float
+ *             example:
+ *               trainId: "12345"
+ *               latitude: 6.927079
+ *               longitude: 79.861244
+ *     responses:
+ *       201:
+ *         description: Train location added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TrainLocation'
+ *       500:
+ *         description: Server error
+ */
 exports.addTrainLocation = async (req, res) => {
   const { trainId, latitude, longitude } = req.body;
 
@@ -13,7 +50,25 @@ exports.addTrainLocation = async (req, res) => {
   }
 };
 
-// Get train locations
+/**
+ * @swagger
+ * /locations:
+ *   get:
+ *     summary: Get all train locations
+ *     description: Retrieve a list of all train locations sorted by timestamp
+ *     tags: [TrainLocation]
+ *     responses:
+ *       200:
+ *         description: A list of train locations
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/TrainLocation'
+ *       500:
+ *         description: Server error
+ */
 exports.getTrainLocations = async (req, res) => {
   try {
     const locations = await TrainLocation.find().sort({ timestamp: -1 });
@@ -23,7 +78,32 @@ exports.getTrainLocations = async (req, res) => {
   }
 };
 
-// Get specific train location
+/**
+ * @swagger
+ * /locations/{id}:
+ *   get:
+ *     summary: Get a specific train location by ID
+ *     description: Retrieve a specific train location by its ID
+ *     tags: [TrainLocation]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the train location
+ *     responses:
+ *       200:
+ *         description: A specific train location
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TrainLocation'
+ *       404:
+ *         description: Location not found
+ *       500:
+ *         description: Server error
+ */
 exports.getTrainLocationById = async (req, res) => {
   try {
     const location = await TrainLocation.findById(req.params.id);
